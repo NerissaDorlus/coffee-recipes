9746a26119448bcddcb72145acc81dcfd89eafd6
Nerissa Dorlus <nerissadorlus@Tdas-Pro-3.local>
2022-05-17 16:28:19 -0400

    added useEffect to fetch Api coffees

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import CCoffee from "./components/CCoffee";
+// import CCoffee from "./components/CCoffee";
 import FCoffee from "./components/FCoffee";
 
 function App() {
   return (
     <>
-      <CCoffee firstName="Nerissa" />
+      {/* <CCoffee firstName="Nerissa" /> */}
       <FCoffee firstName="Nerissa" />
     </>
   );
--- src/components/FCoffee.js
+++ src/components/FCoffee.js
@@ -1,16 +1,33 @@
-import React, { useState } from "react";
-
+import React, { useEffect, useState } from "react";
+// define react component and passes prop
 function FCoffee({ firstName }) {
-  const [count, setCount] = useState(0);
-  const handleClick = () => {
-    setCount(count + 1);
-  };
+  const [coffeeList, setCoffeeList] = useState(); //creating state variable
+  const [temperature, setTemperature] = useState("hot"); //set to make change to state variable
+  useEffect(() => {
+    fetch(`https://api.sampleapis.com/coffee/${temperature}`)
+      .then((response) => response.json()) // waiting for response from api and changing to json
+      .then((data) => setCoffeeList(data)) //.then(setCoffeeList) is also equivalent
+      .catch(console.error);
+  }, [temperature]);
   return (
     <>
       <h1>Coffee List (F)</h1>
+      {/* first name is props which is passed from the parent */}
       <p>Hello {firstName}</p>
-      <p>You clicked the button {count} times.</p>
-      <button onClick={handleClick}>Click Me</button>
+      <button onClick={() => setTemperature("hot")}>HOT 🔥</button>
+      <button onClick={() => setTemperature("iced")}>ICED 🧊</button>
+
+      {!coffeeList ? (
+        <h2> Loading ...</h2>
+      ) : (
+        <>
+          <h2>Coffees</h2>
+          {/* once coffee list is retrieved go through each coffee */}
+          {coffeeList.map((coffee) => {
+            return <p key={coffee.id}>{coffee.title}</p>;
+          })}
+        </>
+      )}
     </>
   );
 }
